922e754132d54aa108fcbb3893ada0b226952edc
Amar <amararya780@gmail.com>
2026-02-06 22:30:24 +0530

    poet added in sitemap

--- backend/src/controllers/sitemap.controller.js
+++ backend/src/controllers/sitemap.controller.js
@@ -1,21 +1,31 @@
 const Poem = require('../models/poem'); 
+const Poet = require('../models/poet'); 
 
 exports.generateSitemap = async (req, res) => {
   try {
     res.setHeader('Content-Type', 'application/xml');
 
     const poems = await Poem.find({}, '_id');
+    const poets = await Poet.find({}, '_id');
 
-    const urls = poems.map(p => `
+    const poemUrls = poems.map(p => `
       <url>
         <loc>https://sukhan-pi.vercel.app/poems/${p._id}</loc>
       </url>
     `).join('');
 
+    const poetUrls = poets.map(p => `
+      <url>
+        <loc>https://sukhan-pi.vercel.app/poets/${p._id}</loc>
+      </url>
+    `).join('');
+
+    const urls = poemUrls + poetUrls;
+
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${urls}
-        </urlset>`;
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls}
+      </urlset>`;
 
     res.status(200).send(xml);
 
